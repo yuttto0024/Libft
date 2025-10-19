cc6396f9b8293209342ff562c75d59763d8c9b31
yuttto0024 <yuto0024@outlook.com>
2025-10-19 14:41:05 +0900

    ft_isascii

---
+++ ft_isascii.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 11:06:21 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/10/19 11:17:02 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <ctype.h>
+
+int	ft_isascii(int c)
+{
+	if (c >= '\0' && c <= 127)
+		return (1);
+	return (0);
+}
+/*
+void	main(void)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	l;
+
+	i = '\0';
+	j = '<';
+	k = -42;
+	printf("---isascii---\n");
+	l = isascii(i);
+	printf("i:%d\n", l);
+	l = isascii(j);
+	printf("j:%d\n", l);
+	l = isascii(k);
+	printf("k:%d\n", l);
+
+	printf("---ft_isascii---\n");
+	l = ft_isascii(i);
+	printf("i:%d\n", l);
+	l = ft_isascii(j);
+	printf("j:%d\n", l);
+	l = ft_isascii(k);
+	printf("k:%d\n", l);
+}
+*/
