49fc31153a678aa39bf77041d5c2ec962d603949
yuttto0024 <yuto0024@outlook.com>
2025-10-19 15:00:55 +0900

    ft_isalnum

---
+++ ft_isalnum.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 10:56:18 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/10/19 11:02:18 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <ctype.h>
+
+int	ft_isalnum(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}lll
+/*
+void	main(void)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	l;
+
+	i = '0';
+	j = 'a';
+	k = '/';
+	printf("---isalnum---\n");
+	l = isalnum(i);
+	printf("i:%d\n", l);
+	l = isalnum(j);
+	printf("j:%d\n", l);
+	l = isalnum(k);
+	printf("k:%d\n", l);
+
+	printf("---ft_isalnum---\n");
+	l = ft_isalnum(i);
+	printf("i:%d\n", l);
+	l = ft_isalnum(j);
+	printf("j:%d\n", l);
+	l = ft_isalnum(k);
+	printf("k:%d\n", l);
+}
+*/
