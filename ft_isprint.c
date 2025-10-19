ca7512d89c6cdab8a150371ffd07d6aa0a939753
yuttto0024 <yuto0024@outlook.com>
2025-10-19 14:36:03 +0900

    ft_isprint

---
+++ ft_isprint.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 11:20:21 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/10/19 11:24:55 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <ctype.h>
+
+int	ft_isprint(int c)
+{
+	if ((c >= '\0' && c <= 31) || c == 127)
+		return (0);
+	return (1);
+}
+/*
+void	main(void)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	l;
+
+	i = '\0';
+	j = '<';
+	k = '!';
+	printf("---isprint---\n");
+	l = isprint(i);
+	printf("i:%d\n", l);
+	l = isprint(j);
+	printf("j:%d\n", l);
+	l = isprint(k);
+	printf("k:%d\n", l);
+
+	printf("---ft_isprint---\n");
+	l = ft_isprint(i);
+	printf("i:%d\n", l);
+	l = ft_isprint(j);
+	printf("j:%d\n", l);
+	l = ft_isprint(k);
+	printf("k:%d\n", l);
+}
+*/
