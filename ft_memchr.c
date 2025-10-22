d7ad6330175a012af33c8001a72c0c1118f79e2b
yuttto0024 <yuto0024@outlook.com>
2025-10-22 21:22:16 +0900

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 20:38:02 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/10/22 21:21:51 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <string.h>
+//#include <stdio.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*t;
+	unsigned char		sc;
+	size_t				i;
+
+	t = (const unsigned char *)s;
+	sc = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (t[i] == sc)
+			return ((void *)&t[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+/*
+int	main(void)
+{
+	char	s[] = "Happy";
+	int		c;
+	size_t	n;
+
+	c = 'p';
+	n = 5;
+	printf("---memchr---\n");
+	printf("%p\n", memchr(s, c, n));
+	printf("---ft_memchr---\n");
+	printf("%p\n", ft_memchr(s, c, n));
+}
+*/
