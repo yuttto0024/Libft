3507498e71307f48912ae350f568d4da33fb5702
yuttto0024 <yuto0024@outlook.com>
2025-10-24 16:30:12 +0900

    ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 15:44:52 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/10/24 16:29:46 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+size_t	ft_strlen(const char *src)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
+	i = 0;
+	while (src[i] && (i < size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
+
+/*
+int	main(void)
+{
+	char	src[] = "Happy";
+	char	dest[5];
+	size_t	buff_size;
+	size_t	return_len;
+
+	buff_size = sizeof(dest);
+
+	printf("---ft_strlcpy---\n");
+	return_len = ft_strlcpy(dest, src, buff_size);
+	printf("dest:%s\n", dest);
+	printf("len:%zu\n", return_len);
+}
+*/
