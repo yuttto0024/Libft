47315656fed13b01785079c3c4e7c82937cdece4
yuttto0024 <yuto0024@outlook.com>
2025-10-25 21:36:38 +0900

    ft_strjoin2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 21:09:39 by yuonishi          #+#    #+#             */
-/*   Updated: 2025/10/25 21:26:57 by yuonishi         ###   ########.fr       */
+/*   Updated: 2025/10/25 21:36:12 by yuonishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,49 @@
 
 size_t	ft_strlen(char const *str)
 {
-	int	len;
+	size_t	len;
 
+	if (str == NULL)
+		return (0);
 	len = 0;
 	while (str[len])
 		len++;
 	return (len);
 }
 
+static void	ft_join_and_copy(char *dst, const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		dst[i] = s2[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+}
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	size;
 	char	*dual;
-	size_t	i;
-	size_t	j;
 
-	size = ft_strlen(s1) + ft_strlen(s2);
-	if (size == 0)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
+	size = ft_strlen(s1) + ft_strlen(s2);
 	dual = (char *)malloc(size + 1);
 	if (dual == NULL)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		dual[i] = s1[i];
-		i++;
-	}
-	j = 0;	
-	while (s2[j])
-	{
-		dual[i] = s2[j];
-		i++;
-		j++;
-	}
-	dual[i] = '\0';
+	ft_join_and_copy(dual, s1, s2);
 	return (dual);
 }
 
@@ -61,4 +69,5 @@ int	main(void)
 
 	p = ft_strjoin(s1, s2);
 	printf("%s", p);
+	free(p);
 }
