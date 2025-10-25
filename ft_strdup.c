dfefff214ba3806428f0100857ea26e696c75506
yuttto0024 <yuto0024@outlook.com>
2025-10-25 14:23:41 +0900

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 13:43:34 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/10/25 14:23:21 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static void	*ft_memcpy(void *dest, const char *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	i = 0;
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	while (n > i)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		len;
+	char	*dest;
+
+	len = ft_strlen(s);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+		return (NULL);
+	ft_memcpy(dest, s, len + 1);
+	return (dest);
+}
+
+/*
+int	main(void)
+{
+	char	src[] = "HappyHappy";
+	char	*s;
+
+	printf("---strdup---\n");
+	s = strdup(src);
+	printf("%s\n", s);
+	free(s);
+	printf("---strdup---\n");
+	s = ft_strdup(src);
+	printf("%s\n", s);
+	free(s);
+}
+*/
