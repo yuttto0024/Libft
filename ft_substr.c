d79cc6c42ab1a0cab2d38eb0234fa6e607b30db1
yuttto0024 <yuto0024@outlook.com>
2025-10-25 20:50:54 +0900

    ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 16:15:59 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/10/25 20:43:18 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *t)
+{
+	size_t	len;
+
+	len = 0;
+	while (t[len])
+		len++;
+	return (len);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	size;
+	size_t	str_len;
+	char	*sub_str;
+	size_t		i;
+	
+	if (s == NULL)
+		return (NULL);
+	str_len = ft_strlen(s);
+
+	if (start > str_len)
+	{
+		sub_str = (char *)malloc(1);
+		if (sub_str == NULL)
+			return (NULL);
+		sub_str[0] = '\0';
+		return (sub_str);
+	}
+
+	size = ft_strlen(s + start);
+	if (size > len)
+		size = len;
+	sub_str = (char *)malloc(size + 1);
+	if (sub_str == NULL)
+		return (NULL);
+	i = 0;
+	while (size > i)
+	{
+		sub_str[i] = s[start + i];		
+		i++;
+	}
+	sub_str[i] = '\0';
+	return (sub_str);
+}
+
+int	main(void)
+{
+	const char		src[] = "HappyHoliday";
+	unsigned int	start;
+	size_t			len;
+	char			*p;
+
+	start = 5;
+	len = 7;
+	p = ft_substr(src, start, len);
+	printf("%s", p);
+	free(p);
+}
