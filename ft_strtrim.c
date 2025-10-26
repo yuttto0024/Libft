09dce442fb49b99fd5d080c387b75055e64e3700
yuttto0024 <yuto0024@outlook.com>
2025-10-26 17:08:39 +0900

    ft_strtrim3

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 16:39:20 by yuonishi          #+#    #+#             */
-/*   Updated: 2025/10/26 16:54:15 by yuonishi         ###   ########.fr       */
+/*   Updated: 2025/10/26 17:07:54 by yuonishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,23 +59,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 		trim = ft_strdup(s1);
 	start = get_start_index(s1, set);
 	if (s1[start] == '\0')
-	{
-		trim = (char *)malloc(1);
-		if (trim == NULL)
-			return (NULL);
-		trim[0] = '\0';
-		return (trim);
-	}
+		return (ft_calloc(1, 1));
 	end = get_end_index(s1, set, start);
 	trim_len = (end - start) + 1;
-	trim = (char *)malloc(end - start + 2);
+	trim = (char *)ft_calloc(end - start + 2, 1);
 	if (trim == NULL)
 		return (NULL);
 	ft_memcpy(trim, s1 + start, trim_len);
-	trim[trim_len] = '\0';
 	return (trim);
 }
-
 /*
 int	main(void)
 {
@@ -85,5 +77,6 @@ int	main(void)
 
 	trim = ft_strtrim(s1, set);
 	printf("%s", trim);
+	free(trim);
 }
 */
