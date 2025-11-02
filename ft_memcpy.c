353ca68fc20a30709f42c2d9ad150aa8e41ad1bf
yuttto0024 <yuto0024@outlook.com>
2025-11-02 21:16:02 +0900

    ft_memcpy2

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,21 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/19 20:59:36 by yuonishi          #+#    #+#             */
-/*   Updated: 2025/11/01 20:19:07 by yuonishi         ###   ########.fr       */
+/*   Created: 2025/11/02 20:33:24 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/11/02 21:13:35 by yuonishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-//#include <stdio.h>
 #include "libft.h"
+// #include <string.h>
+// #include <stdio.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (dest == NULL && src == NULL)
-		return (NULL);
 	i = 0;
 	while (n > i)
 	{
@@ -29,21 +27,19 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-/*
-int main(void)
-{
-	char	src[] = "42 Tokyo!";
-	char	dest1[] = "--------------------"; 
-	char	dest2[] = "--------------------" ;
-	size_t	i;
-	
-	i = 8;
-	printf("---memset---\n");
-	memcpy(dest1, src, i);
-	printf("%s\n", dest1);
-	printf("---ft_memset---\n");
-    ft_memcpy(dest2, src, i);
-	printf("%s\n", dest2);
-	return (0);
-}
-*/
+// int main(void)
+// {
+// 	char	*src = NULL;
+// 	char	*dest1 = NULL; 
+// 	char	*dest2 = NULL;
+// 	size_t	i;	
+// 	i = 7;
+//
+// 	printf("---memset---\n");
+// 	memcpy(dest1, src, i);
+// 	printf("%s\n", dest1);
+// 	// printf("---ft_memset---\n");
+//     // ft_memcpy(dest2, src, i);
+// 	// printf("%s\n", dest2);
+// 	return (0);
+// }
